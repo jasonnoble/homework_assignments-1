1eea9567bd8862b38809623204552928237875a0
kimberlymurphy <murphykimberlyann@gmail.com>
2014-02-17 13:29:38 -0700

    Fixed style issues

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
-str_F = "Fizz"
-str_B = "Buzz"
-count =1
+str_Fizz = "Fizz"
+str_Buzz = "Buzz"
+count = 1
 
-while count<=100
-  if count%3==0 && count%5==0
-    puts str_F+str_B
-  elsif count%3==0
-    puts str_F
-  elsif count%5==0
-    puts str_B
+while count <= 100
+  if count % 3 == 0 && count % 5 == 0
+    puts str_Fizz+str_Buzz
+  elsif count % 3 == 0
+    puts str_Fizz
+  elsif count % 5 == 0
+    puts str_Buzz
   else
     puts count
   end
