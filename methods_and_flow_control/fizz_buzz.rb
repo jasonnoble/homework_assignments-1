b936a9de71010945f2508263d1a91eb9dc12d53b
kimberlymurphy <murphykimberlyann@gmail.com>
2014-02-18 16:15:44 -0700

    Corrections

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 
-str_Fizz = "Fizz"
-str_Buzz = "Buzz"
+str_fizz = "Fizz"
+str_buzz = "Buzz"
 count = 1
 
 while count <= 100
   if count % 3 == 0 && count % 5 == 0
-    puts str_Fizz+str_Buzz
+    puts str_fizz + str_buzz
   elsif count % 3 == 0
-    puts str_Fizz
+    puts str_fizz
   elsif count % 5 == 0
-    puts str_Buzz
+    puts str_buzz
   else
     puts count
   end
--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby
 
-str_Practice = "this is a string to practice with"
+str_practice = "this is a string to practice with"
 
-puts str_Practice
-puts str_Practice.capitalize
-puts str_Practice.upcase
-puts str_Practice.capitalize.gsub("string", "'string'")
-puts "The string '" + str_Practice + "' has " + str_Practice.length.to_s + " characters"
-puts str_Practice.reverse
-str_Practice = str_Practice.split
-puts ((str_Practice[5] + " ") * 3).strip + "!"
+puts str_practice
+puts str_practice.capitalize
+puts str_practice.upcase
+puts str_practice.capitalize.gsub("string", "'string'")
+puts "The string '" + str_practice + "' has " + str_practice.length.to_s + " characters"
+puts str_practice.reverse
+str_practice = str_practice.split
+puts ((str_practice[5] + " ") * 3).strip + "!"
