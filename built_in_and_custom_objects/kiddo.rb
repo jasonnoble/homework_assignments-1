9cda80a8500b29649ffc6a00f6929de979f2b719
kimberlymurphy <murphykimberlyann@gmail.com>
2014-02-19 15:11:41 -0700

    Add disobedience default; refine style

--- built_in_and_custom_objects/kiddo.rb
+++ built_in_and_custom_objects/kiddo.rb
@@ -1,37 +1,40 @@
 #!/usr/bin/env ruby
 
 class Kiddo
-  def initialize(name = "John Doe", topic ="", bedtime = "8")
+  def initialize(name = "John Doe", topic = "", bedtime = "8")
     @given_name = name
+    @first_name = (name.split(" "))[0]
     @interest = topic
     @bedtime = bedtime
  end
 
-  def parental_request(order, complies)
+  def parental_request(order = "", complies = false)
       puts "Parent:  #{@given_name}, please " + order.downcase + "."
       if complies
-        puts "#{@given_name}:  Okay, I will " + order.sub("your", "my").downcase + "."
+        puts "#{@first_name}:  Okay, I will " + order.sub("your", "my").downcase + "."
         reward
       else
-        puts "#{@given_name}:  I DON'T WANNA " + order.sub("your", "my").upcase + "!"
+        puts "#{@first_name}:  I DON'T WANNA " + order.sub("your", "my").upcase + "!"
         reprimand
       end
   end
 
   def reward()
-    puts "Parent:  Thank you #{@given_name}, you may watch a #{@interest} movie on Netflix later."
+    puts "Parent:  Thank you #{@first_name}, you may watch a #{@interest} movie on Netflix later."
   end
 
   def reprimand()
-    puts "Parent:  I wish you had listened #{@given_name}...now you must go to bed at #{@bedtime.to_i-1} o'clock."
+    puts "Parent:  I wish you had listened #{@first_name}...now you must go to bed at #{@bedtime.to_i-1} o'clock."
   end
 end
 
-#Pretty self-explanatory...this output/dialogue is a sample use:
-Ziggy = Kiddo.new("Ziggy", "dinosaur",9)
-Rex = Kiddo.new("Rex", "Starwars")
+if __FILE__ == $0
+  #Pretty self-explanatory...this output/dialogue is a sample use:
+  ziggy = Kiddo.new("Ziggy Hardman Murphy", "dinosaur",9)
+  rex = Kiddo.new("Rex Murphy Hardman", "Starwars")
 
-Ziggy.parental_request("Do your homework", true)
-Rex.parental_request("Wash your hands", true)
-Rex.parental_request("Eat your dinner", false)
-Ziggy.parental_request("Pick up your toys", false)
+  ziggy.parental_request("Do your homework", true)
+  rex.parental_request("Wash your hands", true)
+  rex.parental_request("Eat your dinner", false)
+  ziggy.parental_request("Pick up your toys", false)
+end
